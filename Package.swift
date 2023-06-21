49531953973410f96415c98eafa2c75d734a7c52
Aleksey Berezka <alldmeat@icloud.com>
2023-06-21 11:04:49 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -29,17 +29,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/dodopizza/google-maps-swift-package/releases/download/7.4.0/GoogleMaps.xcframework.zip",
+            url: "https://github.com/dodobrands/google-maps-swift-package/releases/download/7.4.0/GoogleMaps.xcframework.zip",
             checksum: "36ae88850a73a9da82563d0f6053ae3d762f3510e2440034bdc03fc76936eb6f"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/dodopizza/google-maps-swift-package/releases/download/7.4.0/GoogleMapsBase.xcframework.zip",
+            url: "https://github.com/dodobrands/google-maps-swift-package/releases/download/7.4.0/GoogleMapsBase.xcframework.zip",
             checksum: "c412d05613165dd939887288f91205486a93da36eac77dc791e26bdf8f2598f7"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/dodopizza/google-maps-swift-package/releases/download/7.4.0/GoogleMapsCore.xcframework.zip",
+            url: "https://github.com/dodobrands/google-maps-swift-package/releases/download/7.4.0/GoogleMapsCore.xcframework.zip",
             checksum: "4396d4e685d936d2b32fac24e8aa8d59b377067ffc78cab25a622a12cc8ad1b1"
         ),
     ]
